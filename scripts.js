a981b59344fc37bc609930e25a6b42a031e236d1
Irene <irenesis123@gmail.com>
2020-09-19 02:05:09 -0400

    [it] floating selector works for portfolio btns...

--- scripts.js
+++ scripts.js
@@ -1,18 +1,16 @@
+// navigate portfolio
 let graphicDesignButton = document.getElementById("graphic-design-btn");
 let uiUXDesignButton = document.getElementById("uiux-design-btn");
 let graphicDesignSection = document.getElementById("graphic-design");
 let uxUIDesignSection = document.getElementById("uiux-design");
 let slideLine = document.getElementById("slide-line");
-let nav = document.getElementById("nav");
-
+// navigate the nav
 const links = document.querySelectorAll(".link");
 const sections = document.querySelectorAll("section");
 
 function changeLinkState() {
   let index = sections.length;
-
   while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
   links.forEach((link) => link.classList.remove("active"));
   links[index].classList.add("active");
 }
@@ -35,6 +33,10 @@ function togglePortfolio() {
     uiUXDesignButton.classList.remove("active-portfolio");
     graphicDesignSection.style.display = "grid";
     uxUIDesignSection.style.display = "none";
+    $(slideLine).css({
+      width: $(".active-portfolio").width() + 10 + "px",
+      left: "0%",
+    });
   });
 
   uiUXDesignButton.addEventListener("click", function () {
@@ -43,13 +45,22 @@ function togglePortfolio() {
     uiUXDesignButton.classList.add("active-portfolio");
     graphicDesignSection.style.display = "none";
     uxUIDesignSection.style.display = "grid";
+    $(slideLine).css({
+      width: $(".active-portfolio").width() + 10 + "px",
+      left: "100% ",
+    });
   });
 }
 
 $("a").smoothScroll({
-  speed: 1250,
-  // to offset from beginning of where section is
-  //   offset: -100,
+  speed: 1000,
+});
+
+$(document).ready(function () {
+  $(slideLine).css({
+    width: $(".active-portfolio").width() + 10 + "px",
+    left: "0%",
+  });
 });
 
 // getYear();
